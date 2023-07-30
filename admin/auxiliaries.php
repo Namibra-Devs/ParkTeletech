710739f2b727eebd08c055a131c63e4a6ea975f1
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2023-07-30 11:00:38 +0000

    homepage backend integrated

--- admin/auxiliaries.php
+++ admin/auxiliaries.php
@@ -1,81 +1,161 @@
 <?php
 
-    // get company name function
-    function getWebsiteDetails()
-    {
-        include('./admin/database/config.php');
-        $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-            return $row;
-        }
-    }
+// get company name function
+function getWebsiteDetails()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $row) {
+    return $row;
+  }
+}
 
-    // get blogs function 
-    function getBlogs() {
-        include('./admin/database/config.php');
-        $statement = $pdo->prepare("SELECT blog_title, blog_content_short, blog_id, blog_date, photo FROM tbl_blogs");
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $blog) {
-            $blog_title = $blog['blog_title'];
-        $blog_content_short = $blog['blog_content_short'];
-        $blog_image = $blog['photo'];
-        $blog_id = $blog['blog_id'];
-        $blog_date = $blog['blog_date'];
-           echo  '
-            <div class="blog-post"  data-aos="fade-up" data-aos-delay="100">
-            <img src="./assets/uploads/'.$blog_image.'" alt="'.$blog_title.'" class="blog-post__image">
-            <div class="blog-post__content">
-              <p class="blog-post__meta">'.$blog_date.'</p>
-              <h4 class="blog-post__title">'.$blog_title.'</h4>
-              <p>'.$blog_content_short.'</p>
-              <a href="single-blog.php?id='.$blog_id.'" class="blog-post__read-more">Read More</a>
+// get services function
+function getServices()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT name, short_description, photo FROM tbl_service");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $service) {
+
+    $service_name = $service['name'];
+    $short_description = $service['short_description'];
+    $photo = $service['photo'];
+
+    echo  '
+          <div class="service-box">
+                <div class="service-img">
+                    <img src="./assets/uploads/service/' . $photo . '" alt="' . $photo . '">
+                </div>
+                <div class="service-text">
+                    <h2>' . $service_name . '</h2>
+                    <p>' . $short_description . '</p>
+                    <a href="#">Read More <img src="img/Vector.png" alt=""></a>
+                </div>
             </div>
-          </div>
+          ';
+  }
+}
+
+
+// get Expertise Techonology function
+function getExpertise()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT name, description FROM tbl_expertise");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $expertise) {
+
+    $name = $expertise['name'];
+    $description = $expertise['short_description'];
+    echo  '
+          <div class="service-box">
+                <div class="service-img">
+                    <img src="./assets/uploads/service/' . $photo . '" alt="' . $photo . '">
+                </div>
+                <div class="service-text">
+                    <h2>' . $service_name . '</h2>
+                    <p>' . $short_description . '</p>
+                    <a href="#">Read More <img src="img/Vector.png" alt=""></a>
+                </div>
+            </div>
+          ';
+  }
+}
+
+
+// get Testimonials function
+function getTestimonials()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT company, comment FROM tbl_testimonial");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $testimonial) {
+
+
+    echo  '
+    <div class="card">
+    <img src="img/bi_quote.png" alt="">
+    <p>'.$testimonial['comment'].'</p>
+    <h3>'.$testimonial['company'].'</h3>
+</div>
+          ';
+  }
+}
+
+// get blogs function 
+function getBlogs()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT blog_title, blog_content, blog_content_short, blog_id, blog_date, photo FROM tbl_blogs");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $blog) {
+    $blog_title = $blog['blog_title'];
+    $blog_content = $blog['blog_content'];
+    $blog_content_short = $blog['blog_content_short'];
+    $blog_image = $blog['photo'];
+    $blog_id = $blog['blog_id'];
+    $blog_date = $blog['blog_date'];
+
+    echo  '
+    <div class="blog-details">
+    <div class="blog-img">
+        <img src="./assets/uploads/blog/'.$blog_image.'" alt="'.$blog_image.'">
+    </div>
+    <div class="blog-details-text">
+        <p><button type="button" style="background-color: #C2EDCC;">News</button> '.$blog_date.'</p>
+        <h3>'.$blog_title.'</h3>
+        <p>'.$blog_content.'</p>
+    </div>
+</div>
             ';
-        }
+  }
+}
+
+// Social icons function
+function getSocialIcons()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT * FROM tbl_social");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $social) {
+    if ($social['social_url'] != '') {
+      echo '<li><a href="' . $social['social_url'] . '"><i class="' . $social['social_icon'] . '"></i></a></li>';
     }
+  }
+}
 
-    // Social icons function
-    function getSocialIcons() {
-        include('./admin/database/config.php');
-        $statement = $pdo->prepare("SELECT * FROM tbl_social");
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);							
-        foreach ($result as $social) 
-        {
-            if($social['social_url']!='')
-            {
-                echo '<li><a href="'.$social['social_url'].'"><i class="'.$social['social_icon'].'"></i></a></li>';
-            }
-        }
-    }
+function getEvents()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT * FROM tbl_events");
+  $statement->execute();
+  $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    function getEvents() {
-        include('./admin/database/config.php');
-        $statement = $pdo->prepare("SELECT * FROM tbl_events");
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($result as $event) {
 
-        foreach ($result as $event) {
-
-            $event_title         = $event['event_title'];
-            $event_content_short = $event['event_content_short'];
-            $event_venue 		 = $event['event_venue'];
-            $event_date          = $event['event_date'];
-            $event_link          = $event['event_link'];
-            $photo               = $event['photo'];
-            echo '
+    $event_title         = $event['event_title'];
+    $event_content_short = $event['event_content_short'];
+    $event_venue      = $event['event_venue'];
+    $event_date          = $event['event_date'];
+    $event_link          = $event['event_link'];
+    $photo               = $event['photo'];
+    echo '
             <div class="flex-events">
         <div class="flex-event"  data-aos="fade-up" data-aos-delay="100">
           <div class="date">
-            <p class="event-text">'.$event_date.'</p>
+            <p class="event-text">' . $event_date . '</p>
           </div>
           <div class="event-details">
-            <h4 class="text-xl">'.$event_title.'</h4>
-            <p>'.$event_content_short.'</p>
+            <h4 class="text-xl">' . $event_title . '</h4>
+            <p>' . $event_content_short . '</p>
             <div>
 
               <div class="date-flex">
@@ -95,35 +175,33 @@
                     d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
                   </path>
                 </svg>
-                <span>'.$event_venue.'</span>
+                <span>' . $event_venue . '</span>
               </div>
               <div class="button-div">
-                <a href="'.$event_link.'" class="get-ticket">GET YOUR
+                <a href="' . $event_link . '" class="get-ticket">GET YOUR
                   TICKETS</a>
               </div>
             </div>
           </div>
         </div>
         <div class="img-div">
-          <img class="event-img" src="./assets/uploads/'.$photo.'" alt="">
+          <img class="event-img" src="./assets/uploads/' . $photo . '" alt="">
         </div>
       </div>
             ';
-        }
-    }
+  }
+}
 
-    function getGallerySlider() {
-      include('./admin/database/config.php');
-      $statement = $pdo->prepare("SELECT * FROM tbl_photo");
-      $statement->execute();
-      $result = $statement->fetchAll();
+function getGallerySlider()
+{
+  include('./admin/database/config.php');
+  $statement = $pdo->prepare("SELECT * FROM tbl_photo");
+  $statement->execute();
+  $result = $statement->fetchAll();
 
-      foreach ($result as $photo) {
-        echo '
-          <div class="swiper-slide" style="margin-right: 30px"><a href="./assets/uploads/'.$photo['photo_name'].'" class="gallery-lightbox"><img src="./assets/uploads/'.$photo['photo_name'].'" class="img-fluid" alt=""></a></div>
+  foreach ($result as $photo) {
+    echo '
+          <div class="swiper-slide" style="margin-right: 30px"><a href="./assets/uploads/' . $photo['photo_name'] . '" class="gallery-lightbox"><img src="./assets/uploads/' . $photo['photo_name'] . '" class="img-fluid" alt=""></a></div>
         ';
-      }
-    }
-
-
-?>
+  }
+}
--- index.php
+++ index.php
@@ -74,47 +74,7 @@
             <div class="underline"></div>
         </div>
         <div class="services_container">
-            
-            <div class="service-box">
-                <div class="service-img">
-                    <img src="img/service-1.png" alt="">
-                </div>
-                <div class="service-text">
-                    <h2>Cell Tower Design and Construction</h2>
-                    <p>This service involves the comprehensive design and construction of cell towers for
-                        telecommunications
-                        companies. Our company specializes in developing innovative and efficient designs that meet the
-                        specific needs of our clients. Our expert team of engineers and architects collaborates
-                        closely with clients to understand their requirements and create customized solutions.</p>
-                    <a href="#">Read More <img src="img/Vector.png" alt=""></a>
-                </div>
-            </div>
-            <div class="service-box">
-                <div class="service-text">
-                    <h2>Cell Tower Installation</h2>
-                    <p>We offer professional installation services for cell towers, ensuring proper and secure placement
-                        of the tower infrastructure. Our experienced installation team
-                        utilizes specialized equipment and adheres to industry
-                        standards to guarantee reliable and efficient tower installation.</p>
-                    <a href="#">Read More <img src="img/Vector.png" alt=""></a>
-                </div>
-                <div class="service-img">
-                    <img src="img/service-2.png" alt="">
-                </div>
-            </div>
-            <div class="service-box">
-                <div class="service-img">
-                    <img src="img/services-3.png" alt="">
-                </div>
-                <div class="service-text">
-                    <h2>Cell Tower Maintenance and Upgrade</h2>
-                    <p>We provide comprehensive maintenance and upgrade services for existing cell towers. We understand
-                        the critical role that cell towers play in maintaining reliable network coverage,
-                        and our services are designed to enhance tower performance, prolong lifespan, and accommodate
-                        technological advancements.</p>
-                    <a href="#">Read More <img src="img/Vector.png" alt=""></a>
-                </div>
-            </div>
+            <?php getServices() ?>
         </div>
         <button class="btn"> <a href="#">View All <img src="img/Vector.png" alt=""></a></button>
     </section>
@@ -193,41 +153,8 @@
         </div>
         <div class="wrapper">
             <div class="carousel owl-carousel">
-                <div class="card">
-                    <img src="img/bi_quote.png" alt="">
-                    <p>At Park Teletech Africa Limited, excellence in infrastructure
-                        development is their hallmark. They combine their technical prowess, industry knowledge,
-                        and attention to detail to ensure that each project is completed to the highest standards.</p>
-                    <h3>Global Telecoms Ltd</h3>
-                </div>
-                <div class="card">
-                    <img src="img/bi_quote.png" alt="">
-                    <p>They prioritise timely project delivery, without compromising on
-                        quality or safety. By choosing Park Teletech Africa Limited as your trusted partner,
-                        you can expect a seamless and professional experience from start to finish.</p>
-                    <h3>Standard Telecoms</h3>
-                </div>
-                <div class="card">
-                    <img src="img/bi_quote.png" alt="">
-                    <p>At Park Teletech Africa Limited, excellence in infrastructure
-                        development is their hallmark. They combine their technical prowess, industry knowledge,
-                        and attention to detail to ensure that each project is completed to the highest standards.</p>
-                    <h3>Gate Corporation</h3>
-                </div>
-                <div class="card">
-                    <img src="img/bi_quote.png" alt="">
-                    <p>They prioritise timely project delivery, without compromising on
-                        quality or safety. By choosing Park Teletech Africa Limited as your trusted partner,
-                        you can expect a seamless and professional experience from start to finish.</p>
-                    <h3>Globacom Ghana</h3>
-                </div>
-                <div class="card">
-                    <img src="img/bi_quote.png" alt="">
-                    <p>At Park Teletech Africa Limited, excellence in infrastructure
-                        development is their hallmark. They combine their technical prowess, industry knowledge,
-                        and attention to detail to ensure that each project is completed to the highest standards.</p>
-                    <h3>Phoenix Ghana</h3>
-                </div>
+
+                <?php getTestimonials() ?>
             </div>
         </div>
     </section>
@@ -237,41 +164,7 @@
             <div class="underline"></div>
         </div>
         <div class="blog-container">
-            <div class="blog-details">
-                <div class="blog-img">
-                    <img src="img/blog-3.png" alt="">
-                </div>
-                <div class="blog-details-text">
-                    <p><button type="button" style="background-color: #C2EDCC;">News</button> 03/10/2023</p>
-                    <h3>Enhancing Telecommunications Connectivity in Africa</h3>
-                    <p>We delve into the cutting-edge technologies, industry expertise, and commitment needed to meeting
-                        the
-                        highest standards in designing, constructing, and maintaining advanced cell towers.</p>
-                </div>
-            </div>
-            <div class="blog-details">
-                <div class="blog-img">
-                    <img src="img/blog-2.png" alt="">
-                </div>
-                <div class="blog-details-text">
-                    <p><button type="button" style="background-color: #C4DBF5;">News</button> 03/10/2023</p>
-                    <h3>The Importance of Reliable Cell Tower Construction</h3>
-                    <p>Check as we discuss the crucial role of reliable cell tower
-                        construction in ensuring seamless connectivity across Africa.</p>
-                </div>
-            </div>
-            <div class="blog-details">
-                <div class="blog-img">
-                    <img src="img/blod-1.png" alt="">
-                </div>
-                <div class="blog-details-text">
-                    <p><button type="button" style="background-color: #F9E3D3;">News</button> 03/10/2023</p>
-                    <h3>Seamless and Efficient Cell Tower Construction: How Park Teletech Delivers Excellence</h3>
-                    <p>In this blog post, we delve into Park Teletech Africa Limited's approach to delivering excellence
-                        in
-                        cell tower construction. We explore their seamless and efficient process.</p>
-                </div>
-            </div>
+            <?php getBlogs() ?>
         </div>
         <button class="btn"> <a href="#">View All <img src="img/Vector.png" alt=""></a></button>
     </section>
